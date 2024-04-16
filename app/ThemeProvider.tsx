c9995aed017add75d28a66b08ca3a285a3d00ef6
lakansantos <rizesuoracle25@gmail.com>
2024-04-16 13:32:46 +0800

    Added countries

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -55,7 +55,7 @@ const AppThemeProvider: React.FC<ThemeWithProps> = ({children}) => {
         default: darkMode ? 'hsl(207, 26%, 17%)' : 'hsl(0, 0%, 98%)',
       },
       primary: {
-        main: darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)',
+        main: darkMode ? 'hsl(0, 0%, 100%)' : 'hsl(200, 15%, 8%)', // for text
       },
       secondary: {
         main: darkMode ? 'hsl(209, 23%, 22%)' : 'hsl(0, 0%, 100%)',
--- app/modules/countries/Countries.tsx
+++ app/modules/countries/Countries.tsx
@@ -1,4 +1,4 @@
-import {Box} from '@mui/material';
+import {Box, Card, CardContent, CardMedia, Typography} from '@mui/material';
 import {COUNTRIES_API_URL} from 'app/configs/constants';
 import React from 'react';
 
@@ -6,6 +6,13 @@ type Countries = {
   name: {
     common: string;
   };
+  flags: {
+    svg: string;
+    png: string;
+  };
+  region: string;
+  capital: string[];
+  population: number;
 }[];
 const Countries = async () => {
   const response = await fetch(COUNTRIES_API_URL);
@@ -17,10 +24,64 @@ const Countries = async () => {
         maxHeight: '650px',
         minHeight: '80%',
         overflowY: 'auto',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        gap: 3,
+        '&::-webkit-scrollbar': {
+          width: '6px',
+        },
+        '&::-webkit-scrollbar-track': {
+          background: 'secondary.main',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          background: '#888',
+          borderRadius: '10px',
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          background: '#555',
+        },
+        width: '100%',
+        height: '100vh',
       }}
     >
       {data.map((country, index) => {
-        return <p key={index}>{country.name.common}</p>;
+        const {name, population, capital} = country;
+
+        return (
+          <Card
+            sx={{
+              width: {xs: '100%', sm: 230},
+              height: {xs: 500, sm: 300},
+              bgcolor: 'secondary.main',
+            }}
+            key={index}
+          >
+            <CardMedia
+              sx={{
+                height: {xs: 250, sm: 150},
+                width: {xs: '100%', sm: 230},
+                objectFit: 'contain',
+              }}
+              image={country.flags.svg}
+              title="test"
+            />
+            <CardContent>
+              <Typography variant="h6" sx={{mb: 1}} fontWeight={700}>
+                {name.common}
+              </Typography>
+              <Typography variant="body2">
+                Population:
+                {population.toLocaleString()}
+              </Typography>
+              <Typography variant="body2">Region: {country.region}</Typography>
+              <Typography variant="body2">
+                Capital: {!!capital && capital.filter((v) => v).join(', ')}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
       })}
     </Box>
   );
