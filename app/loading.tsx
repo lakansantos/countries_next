1b95513b33adb7dbed515d88ce64bbb2bdd55535
lakansantos <rizesuoracle25@gmail.com>
2024-04-18 11:31:07 +0800

    testing applying loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,25 @@
+'use client';
+import {Box, Typography} from '@mui/material';
+import LinearProgress from '@mui/material/LinearProgress';
 import React from 'react';
 
 export default function Loading() {
-  return <div>loading...</div>;
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        display: 'flex',
+        background: '#333',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box sx={{width: '50%', textAlign: 'center'}}>
+        <Typography variant="body1" color="primary.main">
+          Loading countries...
+        </Typography>
+        <LinearProgress />
+      </Box>
+    </Box>
+  );
 }
